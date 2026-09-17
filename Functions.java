ebcae00114ce0f13d2d80bb6cc6397b9f1f65b40
Khurram Shehzad <khurramshehzad2001@outlook.com>
2026-09-17 14:59:25 +0500

    Functions in java

--- Functions.java
+++ Functions.java
@@ -1,5 +1,9 @@
 public class Functions  {
+    static int sum(int a, int b){
+        return a+b;
+    }
     public static void main(String[] args) {
-
+     int sum = Functions.sum(1, 2);
+     System.out.println(sum);
     }
 }
