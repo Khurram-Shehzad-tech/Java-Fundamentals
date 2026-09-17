d6de4630b59d02f24a490604b89f9f0a712dea11
Khurram Shehzad <khurramshehzad2001@outlook.com>
2026-09-17 12:39:50 +0500

    adding java arrays storing and fetching data

--- Arrays.java
+++ Arrays.java
@@ -1,6 +1,31 @@
 public class Arrays {
     public static void main(String[] args) {
-System.out.println("hello");
+        public class Arrays {
+            public static void main(String[] args) {
+// in java arrays are used to store multiples data of the same type it is used to store large amount of collection of same type data
+                //there are two types of array in java
+                //one dimensional
+                //two dimensional
+
+
+                //************************ 1-D array**********************
+                int[] numbers = new int[10]; // initialization of array
+                //storing data into arrays
+                for (int i = 0; i < numbers.length; i++) {
+                    numbers[i] = i;
+                }
+                System.out.println("Data is store ");
+
+                //fetching data from the arraysgit
+                for (int i = 0; i < numbers.length; i++) {
+                    System.out.println(numbers[i]);
+
+                }
+                System.out.println("Data is displayed ");
+
+            }
+        }
+
 
     }
 }
